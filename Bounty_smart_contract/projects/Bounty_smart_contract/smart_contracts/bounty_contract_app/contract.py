3a6f97680baa550081ced6bd9c542465e2a1836f
Paul-Jonathan2005 <kakani.jonathan@gmail.com>
2025-04-24 22:26:12 +0530

    create create_task and release_reward Smart Contracts

---
+++ Bounty_smart_contract/projects/Bounty_smart_contract/smart_contracts/bounty_contract_app/contract.py
@@ -0,0 +1,45 @@
+from algopy import gtxn
+from algopy import ARC4Contract, arc4, Global, itxn, BoxMap
+
+
+class TaskData(arc4.Struct):
+    company: arc4.Address
+    freelancer: arc4.Address
+    reward: arc4.UInt64
+    
+
+class TaskBountyContract(ARC4Contract):
+    
+    def __init__(self) -> None:
+        self.box_map_struct = BoxMap(arc4.UInt64, TaskData, key_prefix="users")
+
+
+    @arc4.abimethod
+    def create_task(self, payment_txn: gtxn.PaymentTransaction ,task_id: arc4.UInt64, company: arc4.Address, freelancer: arc4.Address, reward: arc4.UInt64) -> None:
+        # Check payment is in group txn 0
+        # payment_txn = gtxn.PaymentTransaction(0)
+        assert payment_txn.receiver == Global.current_application_address
+        assert payment_txn.amount == reward.native
+        assert payment_txn.sender == company.native
+
+        # Save task data in a box
+        task_data = TaskData(
+            company=company,
+            freelancer=freelancer,
+            reward=reward,  
+        )
+        self.box_map_struct[task_id] = task_data.copy()
+
+    @arc4.abimethod
+    def release_reward(self, task_id: arc4.UInt64, caller: arc4.Address) -> None:
+        task_data = self.box_map_struct[task_id].copy()
+        assert caller == task_data.company, "Only company can release"
+
+        itxn.Payment(
+            sender=Global.current_application_address,
+            receiver=task_data.freelancer.native,
+            amount=task_data.reward.native
+        ).submit()
+
+        # Optionally delete the task box
+        del self.box_map_struct[task_id]
