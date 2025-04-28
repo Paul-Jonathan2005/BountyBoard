dbfd1879f534db27a8a327d7580f27a0f6e9560a
Paul-Jonathan2005 <kakani.jonathan@gmail.com>
2025-04-28 11:55:36 +0530

    Added Formatting to Smart Contracts

--- Bounty_smart_contract/projects/Bounty_smart_contract/smart_contracts/bounty_contract_app/contract.py
+++ Bounty_smart_contract/projects/Bounty_smart_contract/smart_contracts/bounty_contract_app/contract.py
@@ -2,20 +2,26 @@ from algopy import UInt64, gtxn
 from algopy import ARC4Contract, arc4, Global, itxn, BoxMap
 
 
-class TaskData(arc4.Struct, frozen= True):
+class TaskData(arc4.Struct, frozen=True):
     company: arc4.Address
     freelancer: arc4.Address
     reward: arc4.UInt64
-    
+
 
 class TaskBountyContract(ARC4Contract):
-    
+
     def __init__(self) -> None:
         self.box_map_struct = BoxMap(arc4.UInt64, TaskData, key_prefix="users")
 
-
     @arc4.abimethod
-    def create_task(self, payment_txn: gtxn.PaymentTransaction ,task_id: arc4.UInt64, company: arc4.Address, freelancer: arc4.Address, reward: arc4.UInt64) -> None:
+    def create_task(
+        self,
+        payment_txn: gtxn.PaymentTransaction,
+        task_id: arc4.UInt64,
+        company: arc4.Address,
+        freelancer: arc4.Address,
+        reward: arc4.UInt64,
+    ) -> None:
         # Check payment is in group txn 0
         # payment_txn = gtxn.PaymentTransaction(0)
         assert payment_txn.receiver == Global.current_application_address
@@ -26,7 +32,7 @@ class TaskBountyContract(ARC4Contract):
         task_data = TaskData(
             company=company,
             freelancer=freelancer,
-            reward=reward,  
+            reward=reward,
         )
         self.box_map_struct[task_id] = task_data
 
@@ -39,11 +45,10 @@ class TaskBountyContract(ARC4Contract):
             sender=Global.current_application_address,
             receiver=task_data.freelancer.native,
             amount=task_data.reward.native,
-            fee=0
+            fee=0,
         ).submit()
-        
 
         # Optionally delete the task box
         del self.box_map_struct[task_id]
-        
+
         return result.amount
