1830ebb613788d2b3ca08b30fa39ee455077448d
Paul-Jonathan2005 <kakani.jonathan@gmail.com>
2025-05-21 11:46:47 +0530

    Added Header Component

---
+++ Frontend/my-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import AlgorandLogo from "../assets/image.png"
+import '../css/Header.css';
+
+export default function Header() {
+    return (
+        <header>
+            <img src={AlgorandLogo}/>
+            <h1>Bounty Board</h1>
+        </header>
+    )
+}
