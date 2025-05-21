9805126ed17ff1edc98629630f7aa7496377018b
Paul-Jonathan2005 <kakani.jonathan@gmail.com>
2025-05-21 11:47:16 +0530

    Added Footer Component

---
+++ Frontend/my-app/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import '../css/Footer.css';
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>© 2025 Algorand Community. All rights reserved.</p>
+        </footer>
+    );
+}
