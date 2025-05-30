407887ca64ecc0452a05836cffe80227172af1a9
Paul-Jonathan2005 <kakani.jonathan@gmail.com>
2025-05-30 22:14:39 +0530

    Updated ENV CONFIG with App Id and App Address

--- Frontend/my-app/src/Config.js
+++ Frontend/my-app/src/Config.js
@@ -1,4 +1,7 @@
  const env_config = {
     "backend_url" : 'http://localhost:8000/',
+    "smart_contract_app_id" : 738237368,
+    "smart_contract_app_address": "4HQO2QTPCITE2I3Z5KTNDMH2XAN22U7KE64BIF7CZCRP5DF3LE4RQAPQIE"
+
 };
 export default env_config;
