5bfdd91d4ba7d162a72b6fe7340a2dcf7f388867
Aniga Abhi Sathvik Reddy <151656430+abhisathvik@users.noreply.github.com>
2025-05-31 23:17:16 +0530

    Added Chat box

--- Frontend/my-app/src/components/BountyDetails.jsx
+++ Frontend/my-app/src/components/BountyDetails.jsx
@@ -1,28 +1,51 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { fetchBountyDetails, fetchBountyRequests} from '../services/api';
 import { useParams } from 'react-router-dom';
 import '../css/BountyDetails.css'
 import Alert from '../components/Alert';
 import extractErrorMessage from '../utils/extractErrorMessage';
 import { useNavigate } from 'react-router-dom';
-import {sendBountyRequest} from '../services/api';
+import {sendBountyRequest, fetchMessages, postChatMessage} from '../services/api';
 import CandidateTileList from './CandidateTileList';
+import MessageTileList from './MessageTileList';
 
 
 export default function BountyDetails(){
     const { viewerType, bountyId } = useParams();
     const [bountyDetails, setBountyDetails] = useState([]);
     const [bountyRequests, setBountyRequests] = useState([]);
+    const [chatMessages, setChatMessages] = useState([]);
+    const [formData, setFormData] = useState({
+          chat: '',
+        });
     const isFreelancer = viewerType==="freelancer";
     const navigate = useNavigate();
     const [alertMessage, setAlertMessage] = useState('');
     const [showAlert, setShowAlert] = useState(false);
     const [type, setType] = useState("success")
 
+    const chatEndRef = useRef(null);
+    const scrollToBottom = () => {
+      chatEndRef.current?.scrollIntoView({ behavior: 'auto' });
+    };
+
+    useEffect(() => {
+      scrollToBottom();
+    }, [chatMessages]);
+
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+        setFormData(prev => ({ ...prev, [name]: value }));
+    };
+
     const getBountyDetails = async () => {
       try {
         const data = await fetchBountyDetails(bountyId);
         setBountyDetails(data);
+        if (data.is_assigened){
+          const data2 = await fetchMessages(bountyId);
+          setChatMessages(data2)
+        }
       } catch (error) {
         console.error('Failed to fetch bounty details:', error);
       }
@@ -39,6 +62,22 @@ export default function BountyDetails(){
     useEffect(() => {
         getBountyDetails();
       }, []);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          const data = await postChatMessage(formData, bountyId);
+          setFormData(prev => ({ ...prev, chat: '' })); 
+          setTimeout(() => {
+          getBountyDetails();
+          }, 1500);
+        } catch (error) {
+            const msg = extractErrorMessage(error);
+            setAlertMessage(msg);
+            setShowAlert(true);
+            setType("error")
+        }
+    };
 
      const handleRequestBounty = async () => {
         try{
@@ -109,6 +148,33 @@ export default function BountyDetails(){
             <div>
             <button className='bounty-requests' >Bounty Requests</button> 
             <CandidateTileList candidateDetailsList={bountyRequests} setShowAlert={setShowAlert} setType={setType} setAlertMessage={setAlertMessage} reward={bountyDetails.amount} getBountyDetails={getBountyDetails}/>
+            </div>
+        }
+        {
+            bountyDetails.is_assigened  &&
+            <div className="chat-box">
+              <h2 className='chat-box-header'>Chat Box</h2>
+              <div className="chat-messages">
+                <MessageTileList chatMessages={chatMessages} />
+                <div ref={chatEndRef} />
+              </div>
+              <form onSubmit={handleSubmit} className="chat-form">
+                <input
+                  type="text"
+                  name="chat"
+                  placeholder="Type Your Message Here"
+                  value={formData.chat}
+                  onChange={handleChange}
+                  required
+                  className="chat-input"
+                />
+                <button
+                  type="submit"
+                  className="chat-button"
+                >
+                  Send
+                </button>
+              </form>
             </div>
         }
         </>
