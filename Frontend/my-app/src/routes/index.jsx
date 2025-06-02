a5ea6de5b84e2f6ab5f4630fb92eee5d505a3c1c
Aniga Abhi Sathvik Reddy <151656430+abhisathvik@users.noreply.github.com>
2025-06-02 22:39:34 +0530

    Added Pages in Routing

--- Frontend/my-app/src/routes/index.jsx
+++ Frontend/my-app/src/routes/index.jsx
@@ -8,6 +8,11 @@ import CreatedBountiesPage from '../pages/CreatedBountiesPage';
 import TaskTypeBountiesPage from '../pages/TaskTypeBountiesPage'
 import BountyDetailsPage from '../pages/BountyDetailsPage'
 import AssignedBountiesPage from '../pages/AssignedBountiesPage';
+import FreelancerCompletedBountiesPage from '../pages/FreelancerCompletedBountiesPage';
+import FreelancerPaymentPendingPage from '../pages/FreelancerPaymentPendingPage';
+import ClientCompletedBountiesPage from '../pages/ClientCompletedBountiesPage';
+import ClientPaymentPendingPage from '../pages/ClientPaymentPendingPage';
+import BountyRequestsPage from '../pages/BountyRequestsPage';
 
 export default function AppRoutes() {
   return (
@@ -22,6 +27,11 @@ export default function AppRoutes() {
       <Route path="/freelancer/assigned-bounties" element={<AssignedBountiesPage />} />
       <Route path="/freelancer/bounty-types" element={<BountyTypePage />} />
       <Route path="/freelancer/task-type/:taskType" element={<TaskTypeBountiesPage />} />
+      <Route path="/freelancer/bounty-requests" element={<BountyRequestsPage />} />
+      <Route path="/freelancer/completed-bounties" element={<FreelancerCompletedBountiesPage />} />
+      <Route path="/freelancer/payment-pending" element={<FreelancerPaymentPendingPage />} />
+      <Route path="/client/completed-bounties" element={<ClientCompletedBountiesPage />} />
+      <Route path="/client/payment-pending" element={<ClientPaymentPendingPage />} />
     </Routes>
   );
 }
