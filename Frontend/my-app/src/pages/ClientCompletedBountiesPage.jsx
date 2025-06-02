97ac951df6b79c7e2f8207566e8879b2ba470d65
Aniga Abhi Sathvik Reddy <151656430+abhisathvik@users.noreply.github.com>
2025-06-02 22:35:29 +0530

    Added Client Completed Bounties Page

---
+++ Frontend/my-app/src/pages/ClientCompletedBountiesPage.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import Footer from '../components/Footer.jsx'
+import Header from '../components/Header.jsx'
+import Navbar from '../components/NavBar.jsx'
+import { fetchClientBountyList } from '../services/api.js';
+import BountyTileList from '../components/BountyTileList.jsx';
+import '../css/CreatedBounties.css'
+
+export default function CompletedBountiesPage() {
+    const [bounties, setBounties] = useState([]);
+      useEffect(() => {
+        const getBounties = async () => {
+          try {
+            const data = await fetchClientBountyList('PAID');
+            setBounties(data);
+          } catch (error) {
+            console.error('Failed to fetch bounties:', error);
+          }
+        };
+        getBounties();
+      }, []);
+
+  return (
+    <div id="completedbounties">
+      <Header />
+      <Navbar />
+      <BountyTileList bountyList={bounties} bountyType="PAID" viewerType="client" />
+      <Footer />
+    </div>
+      
+  )
+}
