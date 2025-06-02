87d05c21c14ee6b95ac6f42cf8dc209da3be4d95
Aniga Abhi Sathvik Reddy <151656430+abhisathvik@users.noreply.github.com>
2025-06-02 22:32:57 +0530

    Added Bounty Requests Page in Pages

---
+++ Frontend/my-app/src/pages/BountyRequestsPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from '../components/Footer.jsx'
+import Header from '../components/Header.jsx'
+import Navbar from '../components/NavBar.jsx'
+
+export default function BountyRequestsPage() {
+  return (
+    <div id="bountyrequests">
+      <Header />
+      <Navbar />
+      {/* < /> */}
+      <Footer />
+    </div>
+      
+  )
+}
