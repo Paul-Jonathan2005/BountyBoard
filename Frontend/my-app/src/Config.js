cf7cb7c58e26b81bf43b3c2a2c7f464d8ee49267
Paul-Jonathan2005 <kakani.jonathan@gmail.com>
2025-06-05 09:01:31 +0530

    Added Smart Contract App Fee

--- Frontend/my-app/src/Config.js
+++ Frontend/my-app/src/Config.js
@@ -1,7 +1,8 @@
  const env_config = {
     "backend_url" : 'http://localhost:8000/',
-    "smart_contract_app_id" : 740568050,
-    "smart_contract_app_address": "CYUHZKUJWGSJEWXPTIVREAVOFYMCIT2JJCWHLAW3XMULACBCYURJLYUD2I"
+    "smart_contract_app_id" : 740602454,
+    "smart_contract_app_address": "POQ6OMEW2CKNJFYJVMAMGXB52IVTQVSQG7OU7X4UYIHM2SWQ2UMVFPCT34",
+    "smart_contract_app_fee": 100000
 
 };
 export default env_config;
