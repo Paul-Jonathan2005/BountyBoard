deedd4734cb412b80f85cdfd5d89bb2fa988ba31
Paul-Jonathan2005 <kakani.jonathan@gmail.com>
2025-06-06 11:56:16 +0530

    Added New Pages in App Routes

--- Frontend/my-app/src/routes/index.jsx
+++ Frontend/my-app/src/routes/index.jsx
@@ -16,6 +16,8 @@ import BountyRequestsPage from '../pages/BountyRequestsPage';
 import BountyDisputePage from '../pages/BountyDisputePage';
 import BountyVotingRewardPage from '../pages/BountyVotingRewardPage';
 import PrivateRoute from './PrivateRoute';
+import FreelancerDashboardPage from '../pages/FreelancerDashboardpage';
+import ClientDashboardPage from '../pages/ClientDashboardPage';
 
 export default function AppRoutes() {
   return (
@@ -28,6 +30,8 @@ export default function AppRoutes() {
       <Route path="/:viewerType/bounty-details/:bountyId" element={<PrivateRoute><BountyDetailsPage /></PrivateRoute>} />
       <Route path="/client/created-bounties" element={<PrivateRoute><CreatedBountiesPage /></PrivateRoute>} />
       <Route path="/freelancer/assigned-bounties" element={<PrivateRoute><AssignedBountiesPage /></PrivateRoute>} />
+      <Route path="/:viewerType/freelancer-dashboard" element={<PrivateRoute><FreelancerDashboardPage /></PrivateRoute>} />
+      <Route path="/:viewerType/client-dashboard" element={<PrivateRoute><ClientDashboardPage /></PrivateRoute>} />
       <Route path="/freelancer/bounty-types" element={<PrivateRoute><BountyTypePage /></PrivateRoute>} />
       <Route path="/freelancer/task-type/:taskType" element={<PrivateRoute><TaskTypeBountiesPage /></PrivateRoute>} />
       <Route path="/freelancer/bounty-requests" element={<PrivateRoute><BountyRequestsPage /></PrivateRoute>} />
