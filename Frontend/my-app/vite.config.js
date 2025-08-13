b2df3265acfbc90ae4a256611148c8a357621d72
Paul-Jonathan2005 <kakani.jonathan@gmail.com>
2025-08-14 02:17:14 +0530

    Added Host

--- Frontend/my-app/vite.config.js
+++ Frontend/my-app/vite.config.js
@@ -12,6 +12,9 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
-    }
+    },
+    host: '0.0.0.0',
+    port: 5173
+
   }
 });
